c6890be51f87ea66ef9d9fb6203ccad4816dbe64
seanotieno <seanotieno5@gmail.com>
2024-10-07 19:21:06 +0300

    cube

---
+++ cube 1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<maths.h>
+int main(){
+    int initial,final,cube;
+    printf("enter the initial and final value");
+    scanf("%d%d",&initial,&final);
+
+    if(start<=final{
+        for(initial;initial<=final;initial++){
+            cube=pow(initial,3);
+            printf("the cube of %d is :%d\n",initial,cube);
+        }
+    else{
+        printf("error");
+    }
+    return 0;
+}
